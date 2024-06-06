7585de20824688a97d9fdfa9213687ebd8c67e40
Timm Kluth <tfkluth@gmail.com>
2024-06-07 00:09:07 +0200

    ArrayAccess um Layoutausgabe erweitert + Korrektur Titel

--- examples/arrayaccess.php
+++ examples/arrayaccess.php
@@ -1,5 +1,11 @@
 <?php
 
+
+include('pages/header.php'); 
+require_once('pages/layout.php'); 
+
+$layout = new Layouthelper();
+
 class Entry implements ArrayAccess
 {
 
@@ -47,20 +53,80 @@ $entry = new Entry();
 
 
 
+$code = 'class Entry implements ArrayAccess
+{
+
+
+    public $beispiel = "Beispiel";
+    public $setMe;
+
+    public function offsetExists($offset) 
+    {
+        if($offset == "content") {
+            return true;  
+        }
+        var_dump("offsetExists: {$offset}");
+    }
+        
+
+    public function offsetGet($offset)
+    {
+       
+        if($offset == "content") {
+            return $this->beispiel;
+        }
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if($offset == "content") {
+            $this->setMe = $value;
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+        if($offset == "content") 
+        {
+            unset($this->beispiel);
+
+        }
+    } 
+
+}';
+
+?>
+
+
+
+<h1>ArrayAccess Beispiel</h1>
+<div class="container">
+
+<?php
+
+echo $layout->cardStart();
+echo $layout->formatCode($code);
+echo $layout->cardEnd();
+
+echo $layout->cardStart();
 echo "<b>offsetExists:</b>:";
 var_dump(isset($entry['content']));
-echo "<br /><br />";
+echo $layout->cardEnd();
 
+echo $layout->cardStart();
 echo "<b>offsetGet</b>:";
 var_dump($entry['content']);
-echo "<br /><br />";
+echo $layout->cardEnd();
 
+echo $layout->cardStart();
 $entry["content"] = "trigger";
 echo "<b>offsetSet</b>: {$entry->setMe}";
-echo "<br /><br />";
+echo $layout->cardEnd();
 
+echo $layout->cardStart();
 echo "<b>offsetUnset: (muss leer sein)</b>:";
 unset($entry['content']);
+echo $layout->cardEnd();
 
 // [OffsetExists] wird bei Abfragen wie isset() getriggert
 // [OffsetGet] wird bei "Ausgabe"-Anweisungen getriggert wie var_dump(); 
@@ -68,3 +134,5 @@ unset($entry['content']);
 // [OffsetUnset] vernichtet Variable bei unset 
 
 ?>
+
+<?php include('pages/footer.php'); ?>
