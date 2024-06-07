4a962bc38f91c3eb2e856e5c863c8984eee8ae7d
Timm Kluth <tfkluth@gmail.com>
2024-06-07 12:44:30 +0200

    variabler Methodenname und Aufruf noch einmal demonstriert.

--- examples/variableattributes.php
+++ examples/variableattributes.php
@@ -5,14 +5,19 @@ class Beispiel
 
     public $eigenschaft = "Ich bin eine Eigenschaft";
 
-
+    public function beispielMethode() {
+        var_dump("Geht auch mit Methoden");
+    }
 }
 
 
 $beispielObj = new Beispiel();
 $name = "eigenschaft";
+$methode = "beispielMethode";
 
 var_dump($beispielObj->$name);
+echo "<br /><br />";
+$beispielObj->$methode();
 
 
 ?>
