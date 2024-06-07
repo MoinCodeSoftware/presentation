1594c0ed145f7c10e713e07129af7511621ee612
Timm Kluth <tfkluth@gmail.com>
2024-06-07 16:22:16 +0200

    Beispiele mit Exceptions und eigen definierten Exceptions im tryCatch-Block

---
+++ examples/exceptions.php
@@ -0,0 +1,62 @@
+<?php
+
+
+
+class MeineException extends Exception
+{
+
+
+
+}
+
+
+function turn($a) {
+
+    if($a == 0) {
+        throw new Exception("Error Exeption");
+    }
+    
+    if($a == 1) {
+        throw new MeineException("Error Meine Exeption");
+    }
+    return -$a;
+}
+
+
+
+    var_dump("START");
+ 
+    try {
+        turn(0);
+    } catch(MeineException $e) {
+        
+        var_dump("Es ist eine MeineException aufgetreten");
+    } catch(Exception $e) {
+        
+        var_dump("Es ist eine Exception aufgetreten");
+    }
+
+
+   var_dump("STOP");
+
+
+   
+   var_dump("START");
+ 
+   try {
+       turn(1);
+   } catch(MeineException $e) {
+       
+       var_dump("Es ist eine MeineException aufgetreten");
+   } catch(Exception $e) {
+       
+       var_dump("Es ist eine Exception aufgetreten");
+   }
+
+
+  var_dump("STOP");
+
+
+
+
+?>
