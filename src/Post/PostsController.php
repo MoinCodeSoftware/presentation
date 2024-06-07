ca3fe1e0ab2d55a5fdab517903a02e0c54699434
Timm Kluth <tfkluth@gmail.com>
2024-06-07 15:58:25 +0200

    Render Methode zum sicherstellen dass params übergeben werden

--- src/Post/PostsController.php
+++ src/Post/PostsController.php
@@ -11,23 +11,44 @@ class PostsController
     }
 
 
+    protected function render($view, $params) 
+    {
 
+   /*      $name = "params";
+        var_dump(${$name}); */
+
+        foreach($params as $key => $value) {
+            ${$key} = $value;
+        }
+
+
+        include __DIR__ . "/../../views/{$view}.php";
+
+
+    }
 
     public function index() 
     {
 
         $posts = $this->postsRepository->fetchPosts();
 
-        include __DIR__ . "/../../views/post/index.php";
+        $this->render("post/index", [
+            'posts' => $posts
+        ]);
+
+      
 
     }
 
     public function show() 
     {
         $id = $_GET['id'];
-        
+
         $post = $this->postsRepository->fetchPost($id);
-        include __DIR__ . "/../../views/post/show.php";
+        $this->render("post/show", [
+            'post' => $post
+        ]);
+
     }
 
 }
