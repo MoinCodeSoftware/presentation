d6047e31a597d88f89f1aad8758b4a5422d9d3da
Timm Kluth <tfkluth@gmail.com>
2024-06-07 11:45:47 +0200

    PostModel hinzugefügt und ArrayAccess Interface implementiert.

--- src/Post/PostModel.php
+++ src/Post/PostModel.php
@@ -9,7 +9,23 @@ class PostModel
     public $id;
     public $title;
     public $content;
+    
 
+    public function getShortContent($content) 
+    {
+        
+        if (strlen($content) <= 120) {
+            return;
+        } else {
+            $shortcontent = preg_replace( '/[^ ]*$/', '', substr( $content, 0, 120 ) );
+            $shortcontentnl = nl2br($shortcontent);
+            $shortcontentout = '<div class="panel-body"><p><b>' . $shortcontentnl . '</b> ... </p></div>';
+
+            return $shortcontentout;
+        }
+ 
+        
+    }
 
 
 }
